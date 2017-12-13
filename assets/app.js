805f7209ef9e09b7e93b959544d729073aa7b09b
Ian Hutchinson <ian@ihut.cc>
2017-12-13 16:15:53 +0000

    New prototype interactions for mobile

--- assets/app.js
+++ assets/app.js
@@ -1,184 +1,149 @@
 $(function() {
-  const TEMPLATE = '<div class="large-4 small-12 cell"><a href="/patterns/{{ slug }}" class="pattern-block"><div class="image">{{ image }}</div><p class="title">{{ title }}</p></a></div>';
-  const DESKTOP_WIDTH = 720;
+  // Runtime variables
+  var isFixedHeaderVisible = false;
+  var prevScrollTop = 0;
+  var isMenuActive = false;
+  var isCategoryMenuActive = false;
+  var menuScrollTop = 0;
+  var menuTimeout;
 
-  $('.category-nav a').click(function(e) {
+  function setInactiveTimeout() {
+    menuTimeout = setTimeout(function() {
+      $('.category-nav-button, header').fadeOut(250);
+    }, 3000);
+  }
+
+  // Category floating menu
+  $('.category-nav-dismiss').click(function(e) {
     e.preventDefault();
 
-    var targetTitle = $(this).text();
-    var targetSlug = $(this).text().toLowerCase().replace(' ', '-');
-
-    $('#patterns-area').animate({
-      'opacity': 0.25
-    }, 100, function() {
-      var jsonPath;
-
-      if (targetSlug === "all") {
-        jsonPath = "/all.json";
-      } else {
-        jsonPath = "/category_json/" + targetSlug + ".json"
-      }
-
-      $.getJSON(jsonPath, function(data) {
-        var patterns = data.patterns.slice(0, -1);
-
-        $('#patterns-area').empty();
-
-        for (var pattern in patterns) {
-          pattern = patterns[pattern];
-
-          var patternHtml = TEMPLATE;
-
-          patternHtml = patternHtml.replace("{{ slug }}", pattern.slug);
-          patternHtml = patternHtml.replace("{{ image }}", '<img src="' + pattern.image + '" />');
-          patternHtml = patternHtml.replace("{{ title }}", pattern.title);
-          patternHtml = patternHtml.replace("{{ description }}", pattern.description);
-
-          $('#patterns-area').append(patternHtml);
-        }
-
-        $('#patterns-area').animate({
-          'opacity': 1
-        }, 100);
-
-        var newTitle = targetTitle + " - Data Permissions Catalogue - IF";
-
-        $(document).attr("title", newTitle);
-        history.pushState(null, newTitle, "/category/" + targetSlug);
-      });
-    });
-
-    $('.category-nav li').removeClass('active');
-    $(this).parent().addClass('active');
+    closeCategoryMenu();
   });
 
-  var isMenuOpen = false;
+  function openCategoryMenu() {
+    $('.category-nav ul').fadeIn(250);
+    $('.category-nav').addClass('active');
+    $('.category-nav-dismiss').show();
 
-  $('.mobile-menu-link').click(function(e) {
+    clearTimeout(menuTimeout);
+
+    isCategoryMenuActive = true;
+  }
+
+  function closeCategoryMenu() {
+    $('.category-nav ul').fadeOut(250);
+    $('.category-nav').removeClass('active');
+    $('.category-nav-dismiss').hide();
+
+    isCategoryMenuActive = false;
+
+    setInactiveTimeout();
+  }
+
+  $('.category-nav-button').click(function(e) {
     e.preventDefault();
 
-    if (isMenuOpen) {
+    if (isCategoryMenuActive) {
+      closeCategoryMenu();
+    } else {
+      openCategoryMenu();
+    }
+  });
+
+  // Mobile menu
+  $('header .menu').click(function(e) {
+    e.preventDefault();
+
+    clearTimeout(menuTimeout);
+
+    if (isMenuActive) {
+      isMenuActive = false
+      $('html, body').scrollTop(menuScrollTop);
+
+      $('.page-menu').fadeOut(250);
+
       $('header').animate({
-        'background-color': '#FFF',
-        'border-bottom-color': '#f1f0ee'
+        'background-color': 'rgba(255, 255, 255, 0.0)'
       }, 250, function() {
         $('header').attr('style', '');
       });
 
-      $('header a').animate({
-        'color': '#212121'
-      }, 250, function() {
-        $('header a').attr('style', '');
-      });
-
-      $('header .logo').removeClass('white');
-
-      $('#mobile-menu').fadeOut(250, function() {
-        $('.mobile-menu-link').text("Menu");
-        isMenuOpen = false;
-      });
+      $('header .menu').removeClass('menu-opened');
     } else {
-      $('header').css({
-        'position': 'fixed'
-      }).animate({
-        'background-color': '#ff5e23',
-        'border-bottom-color': '#ff5e23'
-      }, 250);
+      isMenuActive = true;
+      menuScrollTop = $(window).scrollTop();
 
-      $('header a').animate({
-        'color': '#FFF'
-      }, 250);
+      $('.page-menu').fadeIn(250);
 
-      $('header .logo').addClass('white');
-
-      $('#mobile-menu').fadeIn(250, function() {
-        $('.mobile-menu-link').text("Close");
-        isMenuOpen = true;
+      $('header').animate({
+        'background-color': '#FFF'
+      }, 250).css({
+        'position': 'fixed',
+        'top': 0,
+        'left': 0
       });
+
+      $('header .menu').addClass('menu-opened');
     }
   });
 
-  // Carousel
-  var currentImage = 0;
-  var numImages = 0;
-  var carouselActive = false;
-
-  function createCarousel() {
-    numImages = $('.pattern-image').find('.cell').length;
-
-    if (numImages <= 1 || (numImages > 1 && $(window).width() > DESKTOP_WIDTH)) {
+  // Header interaction
+  $(window).scroll(function() {
+    if (isMenuActive || isCategoryMenuActive) {
       return;
     }
 
-    for (var i = 0; i < numImages; i++) {
-      $('.carousel-indicator .wrapper').append('<a href="#" class="dot">dot</a>');
+    var scrollTop = $(window).scrollTop();
+
+    // Menu timeout
+    clearTimeout(menuTimeout);
+
+    if (scrollTop > $('.homepage-intro').outerHeight() / 2) {
+      setInactiveTimeout();
     }
 
-    $('.carousel-indicator .dot').first().addClass('active');
-
-    $('.pattern-image .cell').not(':first').hide();
-    $('.carousel-indicator').css({
-      'opacity': 0,
-      'display': 'flex'
-    });
-    $('.carousel-control').fadeIn(250);
-    $('.carousel-indicator').animate({
-      'opacity': 1
-    }, 250);
-
-    carouselActive = true;
-  }
-
-  function changeImage() {
-    $('.pattern-image .cell').hide();
-    $('.pattern-image .cell').eq(currentImage).show();
-    $('.carousel-indicator .dot').removeClass('active');
-    $('.carousel-indicator .dot').eq(currentImage).addClass('active');
-  }
-
-  function removeCarousel() {
-    currentImage = 0;
-    numImages = 0;
-    carouselActive = false;
-
-    $('.pattern-image .cell').show();
-    $('.carousel-indicator .wrapper').empty();
-    $('.carousel-control, .carousel-indicator').hide();
-  }
-
-  $('.carousel-control.previous').click(function(e) {
-    e.preventDefault();
-
-    if (currentImage == 0) {
-      return;
+    // Show category menu button when scrolling down,
+    // and half way past homepage header
+    if (scrollTop > ($('.homepage-intro').outerHeight() / 2)) {
+      if (scrollTop > prevScrollTop) {
+        $('.category-nav-button').fadeIn(250);
+      }
     }
 
-    currentImage--;
+    // When past the homepage header
+    if (scrollTop > $('.homepage-intro').outerHeight()) {
+      if (scrollTop > prevScrollTop) {
+        // Scrolling down
+        // Hide header if visible
+        if ($('header').is(':visible')) {
+          $('header').fadeOut(250, function() {
+            $('header').removeClass('fixed');
+          });
+        }
+      } else {
+        // Scrolling up
+        // Fade in header
+        $('header').addClass('fixed').fadeIn(250);
 
-    changeImage();
+        // Show category menu button
+        $('.category-nav-button').fadeOut(250);
+      }
+    }
+
+    if (scrollTop < ($('.homepage-intro').outerHeight()) && scrollTop > ($('.homepage-intro').outerHeight() / 2) && scrollTop < prevScrollTop) {
+      $('header').fadeOut(250);
+      $('.category-nav-button').clearQueue().fadeOut(250);
+    }
+
+    if (scrollTop < ($('.homepage-intro').outerHeight() / 2) && scrollTop < prevScrollTop) {
+      $('header').removeClass('fixed').attr('style', '');
+    }
+
+    // Hide category when at bottom of page
+    if (scrollTop + $(window).height() === $('html').height()) {
+      $('.category-nav-button').fadeOut(250);
+    }
+
+    prevScrollTop = scrollTop;
   });
-
-  $('.carousel-control.next').click(function(e) {
-    e.preventDefault();
-
-    if (currentImage == numImages - 1) {
-      return;
-    }
-
-    currentImage++;
-
-    changeImage();
-  });
-
-  $(window).resize(function() {
-    if (($(window).width() > DESKTOP_WIDTH) && carouselActive) {
-      removeCarousel();
-    } else if (($(window).width() < DESKTOP_WIDTH) && !carouselActive) {
-      createCarousel();
-    }
-  });
-
-  if ($(window).width() < DESKTOP_WIDTH) {
-    createCarousel();
-  }
 });
