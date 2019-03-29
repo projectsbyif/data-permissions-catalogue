29f2d6d9193354c75e613d596cd3ddda36ce3d05
Emily Mattiussi <emily@projectsbyif.com>
2019-03-29 16:53:04 +0000

    Category nav filters pattern categories on homepage
    This commit sets up the pattern category nav to filter which categories are displayed on homepage.
    
    Also hides 'All' category and instead displays all other (actual) categories by default.

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,37 @@
+/* Pattern category nav on homepage */
+
+$('details ul').click(function(e) {
+  e.preventDefault()
+  const targetLink = e.target
+
+  if (!$(targetLink).is("a")) {
+    return;
+  }
+
+  const categoryToShow = targetLink.classList[0];
+
+  if (categoryToShow == "all") {
+    $('.category-list').show()
+  } else {
+    $('.category-list').hide()
+    $(`.category-list.${categoryToShow}`).show()
+  }
+
+  $('details ul a.active').removeClass('active')
+  $(targetLink).addClass('active')
+})
+
 /* Pattern list by category on homepage */
 
 $('.show-all-button').show()
 
-// Hide patterns on page load
-
+// Hide extra patterns on page load
 $('.category-list ul').each(function(index, category) {
   let patternsList = $(category).children()
   togglePatternsListView(patternsList)
 })
 
-// Show/hide patterns on button click
+// Show/hide extra patterns on button click
 $('.show-all-button').click(function(e){
   e.preventDefault()
 
@@ -21,13 +43,15 @@ $('.show-all-button').click(function(e){
 })
 
 
-function togglePatternsListView(patternsList) {
+function togglePatternsListView(patternsList, currentShowAllButton) {
   if (patternsList.length <= 4) {
     return;
   }
   for (let i = 4; i < patternsList.length; i++) {
     $(patternsList[i]).toggle()
   }
+
+  // TODO - toggle button text btw 'Show all' and 'Hide all'
 }
 
 /* Pattern page feedback form */
