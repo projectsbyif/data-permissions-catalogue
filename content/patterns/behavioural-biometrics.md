c6308731a6b27e525e2c81fc974d07cb097a8504
Emily Mattiussi <emily@projectsbyif.com>
2019-04-03 12:31:27 +0100

    Add JS and finish pattern page and homepage layout
    Added feedback bar to pattern page and category nav to homepage. Added in JS site to handle feedback and showing/hiding
    patterns on homepage.
    
    Public folder was committed initially - added this gitignore in previous commit and removed it from git index in this commit.

--- content/patterns/behavioural-biometrics.md
+++ content/patterns/behavioural-biometrics.md
@@ -1,6 +1,4 @@
 ---
-
-
 title: Behavioural biometrics
 
 category: Authentication
---
+++ static/js/app.js
@@ -0,0 +1,85 @@
+/* Pattern category nav on homepage */
+
+$('details ul').click(function(e) {
+  e.preventDefault()
+  const targetLink = e.target
+  console.log(targetLink)
+
+  if (!$(targetLink).is("a")) {
+    return;
+  }
+
+  const categoryToShow = targetLink.classList[0];
+
+  if (categoryToShow == "all") {
+    console.log('all')
+    $('.category-list').show()
+  } else {
+    $('.category-list').hide()
+    $(`.category-list.${categoryToShow}`).show()
+  }
+
+  $('details ul a.active').removeClass('active')
+  $(targetLink).addClass('active')
+})
+
+/* Pattern list by category on homepage */
+
+// TODO: Hide show all button when less than 4 patterns.
+$('.show-all-button').show()
+
+// Hide extra patterns on page load
+$('.category-list ul').each(function(index, category) {
+  let patternsList = $(category).children()
+  togglePatternsListView(patternsList)
+})
+
+// Show/hide extra patterns on button click
+$('.category-heading').click(function(e){
+  e.preventDefault()
+
+  const patternsList = Array.from(
+    $(this).parents('.category-list')
+    .find('li')
+  )
+  togglePatternsListView(patternsList)
+})
+
+
+function togglePatternsListView(patternsList, currentShowAllButton) {
+  if (patternsList.length <= 4) {
+    return;
+  }
+  for (let i = 4; i < patternsList.length; i++) {
+    $(patternsList[i]).toggle()
+  }
+
+  // TODO - toggle button text btw 'Show all' and 'Hide all'
+}
+
+/* Pattern page feedback form */
+
+$('#giveDetailedFeedback').click(function(e) {
+  e.preventDefault();
+  $('.detailed-feedback').toggle();
+})
+
+$(".usage-feedback").submit(function(e) {
+  e.preventDefault();
+
+  let $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    $('.feedback-form').toggle();
+    $('.success-message').toggle();
+  });
+});
+
+$(".detailed-feedback").submit(function(e) {
+  e.preventDefault();
+
+  let $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    $('.feedback-form').toggle();
+    $('.success-message').toggle();
+  });
+});
