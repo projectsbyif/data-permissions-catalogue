ba09fcd982034850b0cc28270aa7fff31269df22
Emily Mattiussi <emily@projectsbyif.com>
2019-04-08 12:47:12 +0100

    Add initial sorting and filtering functions on homepage
    Created three different partials showing different homepage pattern list views (by category, most recent first, and alphabetically).
    
    Wrote initial JS to show and hide views depending on what navigation link is clicked.
    
    TODO - Sort out functionality of expand all button as it relates to individual see more/see less buttons in category view.

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,50 @@
 /* Change view on homepage */
 
-// Default - By category
+$('.category-nav a').click(function(e) {
+  e.preventDefault()
 
-// Most recent first
+  $('.homepage-section').hide()
 
-// Alphabetically
+  $('.category-nav a').removeClass('active')
+  $(this).addClass('active')
 
-// Expand all categories
+  const targetLink = this.classList[0]
+
+  switch (targetLink) {
+    case 'view-by-category':
+      $('.category-view').show();
+      $('.expand-all').show()
+      break;
+    case 'view-most-recent':
+      $('.most-recent-view').show();
+      $('.expand-all').hide()
+      break;
+    case 'view-alphabetically':
+      $('.alphabetical-view').show();
+      $('.expand-all').hide()
+      break;
+    case 'expand-all':
+      toggleExpandAllButton(this)
+    default:
+      $('.category-list').show();
+      $('view-by-category').addClass('active')
+      $('.expand-all').show()
+      break;
+  }
+})
+
+function toggleExpandAllButton(expandAllButton) {
+  $('.category-view').show()
+
+  // Show/hide patterns
+  togglePatternsListView($('.category-list .pattern-card'))
+
+  // Change text on individual see more buttons
+  $('.see-more-button').each(function(index, button) {
+      $(button).html().toLowerCase() == 'see more' ? $(button).html('See less') : $(button).html('See more')
+  })
+
+}
 
 /* Pattern category lists on homepage */
 
@@ -22,44 +60,48 @@ $('.category-list').each(function(index, category) {
 
 // Hide extra patterns on page load
 $('.category-list .pattern-card-list').each(function(index, category) {
-  let patternsList = $(category).children()
+  let patternsList = $(category).find('.pattern-card')
   togglePatternsListView(patternsList)
 })
 
 // Show/hide extra patterns on button click
 // TODO: only attach click event to category headings with more than 3 patterns
-$('.category-heading').click(function(e){
+$('.category-view .category-heading').click(function(e){
   e.preventDefault()
   const categoryHeading = this;
 
-  const patternsList = Array.from(
-    $(this).parents('.category-list')
-    .find('.pattern-card-list .cell')
-  )
+  const patternsList = $(this).parents('.category-list')
+    .find('.pattern-card')
 
   togglePatternsListView(patternsList)
   toggleHeadingActiveState(categoryHeading)
 })
 
-
 function togglePatternsListView(patternsList) {
-  if (patternsList.length <= 3) {
-    return;
-  }
-  for (let i = 3; i < patternsList.length; i++) {
-    $(patternsList[i]).toggle()
-  }
+  $(patternsList).each(function(index, pattern){
+    if ($(pattern).hasClass('preview')) {
+      $(pattern).show()
+    } else {
+      $(pattern).toggle()
+    }
+  })
 }
 
 function toggleHeadingActiveState(categoryHeading) {
   // Add active state to header
-  console.log(categoryHeading)
   $(categoryHeading).toggleClass('active');
 
   // Change text in seeMoreButton
   const seeMoreButton = $(categoryHeading).parents('.category-list').find('.see-more-button');
 
-  seeMoreButton.html().toLowerCase() == 'see more' ? seeMoreButton.html('See less') : seeMoreButton.html('See more')
+  let isCategoryExpanded = seeMoreButton.html().toLowerCase() == 'see more'
+
+  if (isCategoryExpanded) {
+    seeMoreButton.html('See less')
+  } else {
+    seeMoreButton.html('See more')
+    $('.expand-all').removeClass('active')
+  }
 }
 
 /* Pattern page feedback form */
