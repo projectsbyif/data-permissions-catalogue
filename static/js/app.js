2e74de3f45dbd4897852d437344015954486ee4d
Emily Mattiussi <emily@projectsbyif.com>
2019-04-09 13:41:29 +0100

    Changed JS controlling expand/collapse of categories on homepage
    Previously, JS would loop through all pattern cards and toggle the display of all non-preview cards. Now, it adds and removes
    a class on the category section. The CSS associated with the this class controls the view of non-preview pattern cards.
    
    This avoids the problem I was running into with the Expand All button on the categories view - the fact that everything was
    toggled was leading to problems when expand all was clicked.

--- static/js/app.js
+++ static/js/app.js
@@ -24,7 +24,9 @@ $('.category-nav a').click(function(e) {
       $('.expand-all').hide()
       break;
     case 'expand-all':
-      toggleExpandAllButton(this)
+      $('.category-view').show()
+      $('.view-by-category').addClass('active')
+      toggleExpandAllButton()
     default:
       $('.category-list').show();
       $('view-by-category').addClass('active')
@@ -33,65 +35,56 @@ $('.category-nav a').click(function(e) {
   }
 })
 
-function toggleExpandAllButton(expandAllButton) {
-  $('.category-view').show()
-
-  // Show/hide patterns
-  togglePatternsListView($('.category-list .pattern-card'))
-
-  // Change text on individual see more buttons
-  $('.see-more-button').each(function(index, button) {
-      $(button).html().toLowerCase() == 'see more' ? $(button).html('See less') : $(button).html('See more')
+function toggleExpandAllButton() {
+  $('.category-list').each(function(index, category){
+    $(category).removeClass('preview')
   })
 
+  // Change text on individual see more buttons
+  // TODO - get rid of toggle and do this all through CSS
+  $('.see-more-button').each(function(index, button) {
+    let isCategoryExpanded = $(button).html().toLowerCase() == 'expand category'
+
+    if (isCategoryExpanded) {
+      $(button).html('Collapse category')
+      // TODO - all content changes should be done here not split accross JS and CSS
+      $(button).addClass('active')
+    }
+  })
 }
 
 /* Pattern category lists on homepage */
 
 // Show see more button on load if more than 3 patterns in category
 $('.category-list').each(function(index, category) {
-  let patternsList = Array.from($(category).find('.pattern-card'))
-  let seeMoreButton = $(category).find('.see-more-button');
+  // Hide extra patterns on page load through CSS
+  $(this).addClass('preview')
+  const patternsList = Array.from($(category).find('.pattern-card'))
+  const seeMoreButton = $(category).find('.see-more-button');
 
   if (patternsList.length > 3) {
     $(seeMoreButton).css('display', 'inline-block')
   }
 })
 
-// Hide extra patterns on page load
-$('.category-list .pattern-card-list').each(function(index, category) {
-  let patternsList = $(category).find('.pattern-card')
-  togglePatternsListView(patternsList)
-})
+
 
 // Show/hide extra patterns on button click
-// TODO: only attach click event to category headings with more than 3 patterns
 $('.category-view .see-more-button').click(function(e){
   e.preventDefault()
   const seeMoreButton = this;
+  const category = $(this).parents('.category-list').toggleClass('preview')
 
-  const patternsList = $(this).parents('.category-list')
-    .find('.pattern-card')
-
-  togglePatternsListView(patternsList)
-  toggleHeadingActiveState(this)
+  toggleSeeMoreButton(this)
 })
-
-function togglePatternsListView(patternsList) {
-  $(patternsList).each(function(index, pattern){
-    if ($(pattern).hasClass('preview')) {
-      $(pattern).show()
-    } else {
-      $(pattern).toggle()
-    }
-  })
-}
-
-function toggleHeadingActiveState(seeMoreButton) {
+// TODO - get rid of toggle and do this all through CSS
+function toggleSeeMoreButton(seeMoreButton) {
   let isCategoryExpanded = $(seeMoreButton).html().toLowerCase() == 'expand category'
 
   if (isCategoryExpanded) {
     $(seeMoreButton).html('Collapse category')
+    // This class changes the arrow
+    // TODO - all content changes should be done here not split accross JS and CSS
     $(seeMoreButton).addClass('active')
   } else {
     $(seeMoreButton).html('Expand category')
